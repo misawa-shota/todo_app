03f42ebffae24814785e36fe80bde9a8deeac2e3
misawa-shota <kaiyou0120@gmail.com>
2023-06-28 23:43:37 +0900

    Todoテーブルの作成、投稿一覧の作成。

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todo_app</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <h1><a href="#">Todo_app</a></h1>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <article>
+            <div>
+                <h2>投稿一覧</h2>
+            </div>
+        </article>
+    </main>
+
+    <footer>
+        <p>&copy; Todo_app All rights reserved.</p>
+    </footer>
+</body>
+</html>
